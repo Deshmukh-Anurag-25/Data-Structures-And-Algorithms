9190781c287c7793344b41c55e959bc38e15e7bc
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-10 20:51:17 +0530

    Iterative Bubble Sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble(vector<int> &nums) {
+  int n = nums.size();
+
+  for (int i = 0; i < n; i++) {
+
+    bool isSorted = true;
+
+    for (int j = 1; j < n - i; j++) {
+      if (nums[j] < nums[j - 1]) {
+        isSorted = false;
+        swap(nums[j], nums[j - 1]);
+      }
+    }
+    if (isSorted)
+      break;
+  }
+}
