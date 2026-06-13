eb3e1cde407567d06d43f6b1439cd51cb7f39809
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-13 14:47:39 +0530

    Maximum depth/height of the binary tree

---
+++ Trees/MaxDepth.cpp
@@ -0,0 +1,13 @@
+#include "Node.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxDepth(Node *root) {
+  if (!root)
+    return 0;
+
+  int leftHeight = maxDepth(root->left);
+  int rightHeight = maxDepth(root->right);
+
+  return 1 + max(leftHeight, rightHeight);
+}
