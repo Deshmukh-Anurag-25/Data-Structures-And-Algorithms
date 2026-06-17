61add18cc6235492b9701263dbf69aea6ebb95b2
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-17 23:23:39 +0530

    iterative selection sort

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &nums) {
+  int n = nums.size();
+  for (int i = 0; i < n; i++) {
+    int mini = i;
+    for (int j = i + 1; j < n; j++) {
+      if (nums[j] < nums[mini]) {
+        mini = j;
+      }
+    }
+
+    if (i != mini)
+      swap(nums[i], nums[mini]);
+  }
+}
