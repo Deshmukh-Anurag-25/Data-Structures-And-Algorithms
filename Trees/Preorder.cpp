71fb1da36b9fd1b9025c3c31fc3069c68dd993c7
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-20 19:49:24 +0530

    preorder traversal

---
+++ Trees/Preorder.cpp
@@ -0,0 +1,17 @@
+#include "Node.h"
+#include <vector>
+using namespace std;
+
+void dfs(Node *root, vector<int> &ans) {
+  if (!root)
+    return;
+  ans.push_back(root->val);
+  dfs(root->left, ans);
+  dfs(root->right, ans);
+}
+
+vector<int> preorder(Node *root) {
+  vector<int> ans;
+  dfs(root, ans);
+  return ans;
+}
