6ecb37db3cf43f76fd033dc88ce043e79e1a9be9
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-20 19:49:03 +0530

    inorder traversal

---
+++ Trees/Inorder.cpp
@@ -0,0 +1,17 @@
+#include "Node.h"
+#include <vector>
+using namespace std;
+
+void dfs(Node *root, vector<int> &ans) {
+  if (!root)
+    return;
+  dfs(root->left, ans);
+  ans.push_back(root->val);
+  dfs(root->right, ans);
+}
+
+vector<int> inorder(Node *root) {
+  vector<int> ans;
+  dfs(root, ans);
+  return ans;
+}
