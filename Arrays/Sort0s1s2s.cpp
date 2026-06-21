f2ae9550836433d7080684b9dcf91ce3899b1d5b
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-21 12:52:05 +0530

    sort an array of 0s, 1s and 2s

---
+++ Arrays/Sort0s1s2s.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void sortZeroOneTwo(vector<int> &nums) {
+  int n = nums.size();
+  int low = 0, mid = 0, high = n - 1;
+
+  while (mid <= high) {
+    if (nums[mid] == 0) {
+      swap(nums[low++], nums[mid++]);
+    } else if (nums[mid] == 1)
+      mid++;
+    else {
+      swap(nums[high--], nums[mid]);
+    }
+  }
+}
