c252a3be4126f0c1a580a6f868dcc7ef27e12fe6
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-23 19:34:15 +0530

    check if a binary tree is symmetrical or not

---
+++ Trees/SymmetricalTree.cpp
@@ -0,0 +1,14 @@
+#include "Node.h"
+
+bool isMirror(Node *p, Node *q) {
+  if (!p && !q)
+    return true;
+  if (!p || !q)
+    return false;
+  if (p->val != q->val)
+    return false;
+
+  return isMirror(p->left, q->right) && isMirror(p->right, q->left);
+}
+
+bool isSymmetrical(Node *root) { return isMirror(root->left, root->right); }
