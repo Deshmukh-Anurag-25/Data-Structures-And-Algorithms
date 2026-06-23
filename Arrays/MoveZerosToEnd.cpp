9b3f4907999b024f99179aa3f03fe05c1b8e894e
Deshmukh-Anurag-25 <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-06-24 00:33:24 +0530

    move all zeroes in array to end

---
+++ Arrays/MoveZerosToEnd.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+void moveZeros(vector<int> &nums) {
+  int n = nums.size();
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    if (nums[i] != 0) {
+      nums[j++] = nums[i];
+    }
+  }
+
+  while (j < n)
+    nums[j++] = 0;
+}
